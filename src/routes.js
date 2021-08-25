6eff9b7ac2fdfd785f37a02fbcd9f4b6fe6ebf49
SadgamS <brayan010.ba@gmail.com>
2021-08-24 22:59:17 -0400

    añadiendo la pag de usuarios

--- src/routes.js
+++ src/routes.js
@@ -39,7 +39,7 @@ const Users = React.lazy(() => import('./views/users/Users'));
 const User = React.lazy(() => import('./views/users/User'));
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
+  { path: '/', exact: true, name: 'Inicio' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/theme', name: 'Theme', component: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', component: Colors },
@@ -77,7 +77,7 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', component: Modals },
   { path: '/notifications/toaster', name: 'Toaster', component: Toaster },
   { path: '/widgets', name: 'Widgets', component: Widgets },
-  { path: '/users', exact: true,  name: 'Users', component: Users },
+  { path: '/usuarios', exact: true,  name: 'Usuarios', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User }
 ];
 
--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import {
   CBadge,
   CCard,
@@ -8,79 +8,88 @@ import {
   CCol,
   CDataTable,
   CRow,
-  CPagination
-} from '@coreui/react'
+  CButton,
+  CPagination,
+} from "@coreui/react";
 
-import usersData from './UsersData'
+import usersData from "./UsersData";
 
-const getBadge = status => {
+const getBadge = (status) => {
   switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
+    case "Active":
+      return "success";
+    case "Inactive":
+      return "secondary";
+    case "Pending":
+      return "warning";
+    case "Banned":
+      return "danger";
+    default:
+      return "primary";
   }
-}
+};
 
 const Users = () => {
-  const history = useHistory()
-  const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
-  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
-  const [page, setPage] = useState(currentPage)
+  const history = useHistory();
+  const queryPage = useLocation().search.match(/page=([0-9]+)/, "");
+  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1);
+  const [page, setPage] = useState(currentPage);
 
-  const pageChange = newPage => {
-    currentPage !== newPage && history.push(`/users?page=${newPage}`)
-  }
+  const pageChange = (newPage) => {
+    currentPage !== newPage && history.push(`/users?page=${newPage}`);
+  };
 
   useEffect(() => {
-    currentPage !== page && setPage(currentPage)
-  }, [currentPage, page])
+    currentPage !== page && setPage(currentPage);
+  }, [currentPage, page]);
 
   return (
     <CRow>
-      <CCol xl={6}>
+      <CCol xl={12}>
+        <CButton className="m-2" color="success">
+         Añadir usuario
+        </CButton>
         <CCard>
           <CCardHeader>
-            Users
-            <small className="text-muted"> example</small>
+            <h5 className="mt-2 font-weight-bold">Usuarios</h5>
           </CCardHeader>
           <CCardBody>
-          <CDataTable
-            items={usersData}
-            fields={[
-              { key: 'name', _classes: 'font-weight-bold' },
-              'registered', 'role', 'status'
-            ]}
-            hover
-            striped
-            itemsPerPage={5}
-            activePage={page}
-            clickableRows
-            onRowClick={(item) => history.push(`/users/${item.id}`)}
-            scopedSlots = {{
-              'status':
-                (item)=>(
+            <CDataTable
+              items={usersData}
+              fields={[
+                "id",
+                { key: "name", _classes: "font-weight-bold", label:"Nombre" },
+                "registered",
+                "role",
+                "status",
+              ]}
+              hover
+              striped
+              itemsPerPage={7}
+              activePage={page}
+              clickableRows
+              pagination
+              onRowClick={(item) => history.push(`/users/${item.id}`)}
+              scopedSlots={{
+                status: (item) => (
                   <td>
-                    <CBadge color={getBadge(item.status)}>
-                      {item.status}
-                    </CBadge>
+                    <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
                   </td>
-                )
-            }}
-          />
-          <CPagination
+                ),
+              }}
+            />
+            {/* <CPagination
             activePage={page}
             onActivePageChange={pageChange}
             pages={5}
             doubleArrows={false} 
             align="center"
-          />
+          /> */}
           </CCardBody>
         </CCard>
       </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
